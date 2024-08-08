a87e4432a8d2b9a75f58218643a97a4b190f7248
JanKoppan <167351439+JanKoppan@users.noreply.github.com>
2024-08-08 15:27:32 +0200

    Add files via upload

---
+++ Čtvrtá výzkumná otázka.sql
@@ -0,0 +1,39 @@
+-- Čtvrtá výzkumná otázka: Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+WITH yearly_avg AS (
+    SELECT 
+        category_code,
+        YEAR(date_from) AS year,
+        AVG(value) AS avg_value
+    FROM 
+        czechia_price
+    GROUP BY 
+        category_code,
+        YEAR(date_from)
+),
+yearly_change AS (
+    SELECT 
+        category_code,
+        year,
+        avg_value,
+        LAG(avg_value) OVER (PARTITION BY category_code ORDER BY year) AS previous_avg_value
+    FROM 
+        yearly_avg
+),
+average_increase AS (
+    SELECT 
+        year,
+        AVG((avg_value / previous_avg_value - 1) * 100) AS average_annual_increase 
+    FROM 
+        yearly_change
+    WHERE 
+        previous_avg_value IS NOT NULL
+    GROUP BY 
+        year
+)
+    SELECT 
+        year,
+        ROUND(average_annual_increase, 2) AS average_annual_increase
+    FROM 
+        average_increase
+    ORDER BY
+        year;
